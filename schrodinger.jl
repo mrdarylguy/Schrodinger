20ec0a3fbb5d3e55fc00a71bd8ba0f90d7ee462a
Daryl Tng <42925677+mrdarylguy@users.noreply.github.com>
2021-10-08 01:51:38 +0800

    Display both plots and integrate

--- schrodinger.jl
+++ schrodinger.jl
@@ -13,6 +13,7 @@ n = 3
 x = 0:0.1:L
 y = ψ.(x,n,L)
 
+
 p1 = (x, 
       y,
       w=3,
@@ -30,3 +31,13 @@ p2 = (x,
       ylabel="ψ^2",
       ylim=(-1, 1)
 )
+
+
+#display both plots
+plot(p1, p2, layout=(1,2), legend=:bottomright, legendfontsize=7)
+
+
+#Using the Gauss Konrod Integration algorithm
+using QuadGK
+g(x) = (√(2.0/L)*sin(Float64(n*π*x)/L))^2
+integral, err = quadgk(x -> g(x), 0, 20, rtol=1e-5)
